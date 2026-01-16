e26288f9ff202d34df67994305a0cc874dd9152c
datagreier2 <aron.lindegard@pm.me>
2026-01-16 14:14:42 +0100

    Redep

--- src/sanity/client.js
+++ src/sanity/client.js
@@ -4,9 +4,11 @@ const projectId = import.meta.env.VITE_SANITY_PROJECT_ID || 'wpjddqhy';
 const dataset = import.meta.env.VITE_SANITY_DATASET || 'innhold';
 const apiVersion = import.meta.env.VITE_SANITY_API_VERSION || '2024-01-01';
 
+const useCdn = import.meta.env.PROD
+
 export const sanityClient = createClient({
   projectId,
   dataset,
   apiVersion,
-  useCdn: true,
+  useCdn,
 });
