4e28a008013ed73c41a9b7e3d4d46c72d0280cbb
datagreier2 <aron.lindegard@pm.me>
2026-02-15 15:44:45 +0100

    fixed hard-coded broken path reulting in asset 404 in deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/act/",
+  base: "/",
   optimizeDeps: {
     entries: ["index.html"],
   },
